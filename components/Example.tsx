217009b3086d8e6d27513c4e840d018e6fcbc393
Noé <ncarl@alphaomega.lu>
2023-05-04 14:41:51 +0200

    init

---
+++ components/Example.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+const Example = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+    </div>
+  );
+};
+
+export default Example;
